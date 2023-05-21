16b3e9cc7cb18ef14de9cda4d44a6749c6a097fe
Ayden Boyko <89430262+ayden-boyko@users.noreply.github.com>
2023-05-20 20:23:31 -0700

    fixed an error with textbox data

--- my-website/src/app/api/email.jsx
+++ my-website/src/app/api/email.jsx
@@ -22,20 +22,20 @@ const ContactMe = () => {
     };
     return (
         <div className="rounded-sm border-black border-2 animate-idle-right w-2/6 h-2/6 bg-green-500 ">
-            <form  ref={form} onSubmit={sendEmail} className='bg-white text-black -translate-x-2 -translate-y-2 rounded-sm border-black border-2 p-2'>
-                <label>Name</label>
+            <form  ref={form} onSubmit={sendEmail} className='bg-white text-black -translate-x-2 -translate-y-2 rounded-sm border-black border-2 p-2 dark:bg-slate-500'>
+                <label className="dark:text-white">Name</label>
                 <br />
-                <input type="text" name="user_name" id="user_name" className="bg-white border-black rounded border text-black"/>
+                <input type="text" name="user_name" id="user_name" className="bg-white border-black rounded border text-black "/>
                 <br />
-                <label>Email</label>
+                <label className="dark:text-white">Email</label>
                 <br />
-                <input  type="email" name="email" id="email" className="bg-white border-black rounded border text-black"/>
+                <input  type="email" name="email" id="email" className="bg-white border-black rounded border text-black "/>
                 <br />
-                <label>Message</label>
+                <label className="dark:text-white">Message</label>
                 <br />
                 <textarea name="message" id="message" className="bg-white border-black rounded border w-2/3"/> 
                 <br />
-                <input type="submit" value="Send" className='transition duration-500 delay-100 rounded-sm border-black border p-1 hover:bg-gray-400'/>
+                <input type="submit" value="Send" className='transition duration-500 delay-100 rounded-sm border-black border p-1 hover:bg-gray-400 bg-white'/>
             </form>
         </div>
     );
