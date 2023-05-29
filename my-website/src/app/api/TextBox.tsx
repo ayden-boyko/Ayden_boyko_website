5cab44c6352a99c638559451e9abb4f2a4fa97eb
Ayden Boyko <89430262+ayden-boyko@users.noreply.github.com>
2023-05-28 19:51:27 -0700

    Revert "changes"
    This reverts commit fcbd7fe88d524928ebea733028a1f27ba8808b78.

--- my-website/src/app/api/TextBox.tsx
+++ my-website/src/app/api/TextBox.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { readFile } from "fs/promises";
-import Image from "next/image";
 
 type textboxinfo = {
     file: string;
@@ -15,60 +14,37 @@ const fileToText = (path: string) => {
     try {
         const filePath = new URL(path, import.meta.url);
         const contents = readFile(filePath, { encoding: 'utf8' });
+        //console.log(contents);
         return contents;
       } catch (error) {
         console.error(error);
     }
 }
 
-const TextBox = (props:textboxinfo) => {
+const TextBox = (text:textboxinfo) => {
     let basestyle1:string = "rounded-sm border-black border-2 animate-idle-left ";
     let basestyle2:string = "rounded-sm border-black border-2 animate-idle-right ";
-    let final_style1:string = basestyle1.concat(props.style.toString());
-    let final_style2:string = basestyle2.concat(props.style.toString());
+    let final_style1:string = basestyle1.concat(text.style.toString());
+    let final_style2:string = basestyle2.concat(text.style.toString());
     switch (true) {
-        case ((props.side === "left") && (props.image === "none")):
+        case ((text.side === "left") && (text.image === "none")):
             return(
                 <div className={final_style1}>
                     <p className='bg-slate-200 text-black translate-x-2 -translate-y-2 rounded-sm border-black border-2 dark:text-white dark:bg-slate-500 px-2 py-2 animate-none'>
-                        {fileToText(props.file)}
+                        {fileToText(text.file)}
                     </p>
                 </div>
             );
-        case ((props.side === "right") && (props.image === "none")):
+        case ((text.side === "right") && (text.image === "none")):
             return(
             <div className={final_style2}>
                 <div className='bg-slate-200 text-black -translate-x-2 -translate-y-2 rounded-sm border-black border-2 dark:text-white dark:bg-slate-500 px-2 py-2 animate-none'>
-                    {fileToText(props.file)}
-                </div>
-            </div>
-            )
-        case ((props.side === "left") && (props.image !== "none")):
-            let leftsecondstyle:string = "rounded-sm border-black border-2 animate-idle-left ";
-            return(
-            <div className={final_style2}>
-                <div className={leftsecondstyle}>
-                    <Image src={props.image} alt="alternatetext"/>
-                    <div className='bg-slate-200 text-black -translate-x-2 -translate-y-2 rounded-sm border-black border-2 dark:text-white dark:bg-slate-500 px-2 py-2 animate-none'>
-                        {fileToText(props.file)}
-                    </div>
-                </div>
-            </div>
-            )
-        case ((props.side === "right") && (props.image !== "none")):
-            let rightsecondstyle:string = "rounded-sm border-black border-2 animate-idle-right";
-            return(
-            <div className={final_style2}>
-                <div className={rightsecondstyle}>
-                    <Image src={props.image} alt="alternatetext" width={100} height={100}/>
-                    <div className='bg-slate-200 text-black -translate-x-2 -translate-y-2 rounded-sm border-black border-2 dark:text-white dark:bg-slate-500 px-2 py-2 animate-none'>
-                        {fileToText(props.file)}
-                    </div>
+                    {fileToText(text.file)}
                 </div>
             </div>
             )
         default:
-            return (<></>)
+            <div>Error</div>
     }
 }
 
