d4dfca20d6c5b0aab7bdd8b157f91ae065bec73e
Ayden Boyko <89430262+ayden-boyko@users.noreply.github.com>
2023-07-02 00:35:05 -0700

    componentization

--- my-website/src/app/components/Imagebox.tsx
+++ my-website/src/app/components/Imagebox.tsx
@@ -29,6 +29,7 @@ const Imagebox = (props: props) => {
       <div className=" bg-white rounded-sm border-black border-2 -translate-y-2 translate-x-2 text-black w-full h-full dark:bg-slate-500 dark:text-white pl-16 pb-2 pr-16 pt-2 gap-x-16">
         {text === null ? <></> : <p>{text}</p>}
         <div className=" flex flex-row">
+            {/**use mapp to display images below */}
           <a
             className=" w-10/12 h-4/6 m-2 py-4 rounded-full"
             href="https://discordapp.com/users/bb_tornado"
@@ -44,20 +45,6 @@ const Imagebox = (props: props) => {
               className="bg-white p-2 rounded-full"
             />
           </a>
-          <a
-            className=" w-10/12 h-4/6 m-2 py-4 rounded-full"
-            href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"
-            aria-label="Linkedin"
-            target="_blank"
-            rel="noopener"
-          >
-            <Image
-              src="/linkedin.svg"
-              alt="linkedin Icon"
-              width={200}
-              height={200}
-              className="bg-white p-2 rounded-full"
-            />
           </a>
         </div>
       </div>
