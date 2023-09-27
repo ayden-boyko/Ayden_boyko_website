f1c2675f4b733f482b7c902cfab15171ed913f73
Ayden Boyko <89430262+ayden-boyko@users.noreply.github.com>
2023-09-27 04:28:17 -0400

    deployed

--- my-website/src/app/page.tsx
+++ my-website/src/app/page.tsx
@@ -133,7 +133,7 @@ export default function Home() {
               }
               projectImage={"/React_client_page.jpeg"}
               projectImageSize={640}
-              projectImageStyle="rounded-sm border-2 border-black m-2 xl:translate-x-1 md:translate-y-4"
+              projectImageStyle="rounded-sm border-2 border-black m-2 xl:translate-x-1 xl:translate-y-0 md:translate-y-4"
               projectLink={"https://github.com/ayden-boyko/React-Timer"}
             ></Projectbox>
           </div>
